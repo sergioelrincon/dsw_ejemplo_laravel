784cabe4fa278b0cbfc14aab462f37b1b41f7e7d
CRPS <cristobits0101@gmail.com>
2024-01-19 23:28:47 +0000

    Finalizado el controlador de productos

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,45 +5,56 @@ namespace App\Http\Controllers;
 class ProductController extends Controller
 {
 
+    // Array asociativo con los productos de la tienda.
     public $products = [
         "1" => [
             "nombre"      => "tv",
             "descripción" => "Televisor Samsung",
-            "url"         => "",
-            "precio"      => "",
+            "url"         => "/img/game.png",
+            "precio"      => "2999,00 €",
         ],
         "2" => [
             "nombre"      => "iPhone",
             "descripción" => "iPhone 15 Plus",
-            "url"         => "",
-            "precio"      => "",
+            "url"         => "/img/safe.png",
+            "precio"      => "1239 €",
         ],
         "3" => [
             "nombre"      => "chromecast",
             "descripción" => "Google TV",
-            "url"         => "",
-            "precio"      => "",
+            "url"         => "/img/submarine.png",
+            "precio"      => "69,99 €",
         ],
         "4" => [
             "nombre"      => "Glasses",
             "descripción" => "Multiópticas gafas",
-            "url"         => "",
-            "precio"      => "",
+            "url"         => "/img/game.png",
+            "precio"      => "74,00 €",
         ],
     ];
 
     /**
-     * The index function returns a view with a list of products.
-     *
-     * @return a view called "product.index" and passing the variable "products" to the view.
+     * La función de índice devuelve una vista con una lista de productos.
+     * 
+     * @return la vista índice de los productos a la cual se le pasa el array productos para que los muestre.
      */
-    public function index() {
-
+    public function index() 
+    {
         return view("product.index")->with("products", $this->products);
     }
 
-    public function show(string $id) {
-
-        return view("product.show")->with("",);
+    /**
+     * Recupera un producto específico de una matriz y lo pasa a una vista para su visualización.
+     * 
+     * @param string $id El identificador del producto que se mostrará.
+     * 
+     * @return una vista llamada show que recibe el producto de la petición para que lo serialice.
+     */
+    public function show(string $id) 
+    {
+        foreach ($this->products as $key => $value)
+            $product = ($key == $id) ? $value : null;
+    
+        return view("product.show")->with("product", $product);
     }
 }
