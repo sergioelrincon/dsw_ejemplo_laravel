5bfe605eec4e6aa6922ae534d3bae101df827c32
Cristo RPS <cristobits0101@gmail.com>
2024-01-25 11:59:55 +0000

    Añadiendo refactorización

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,33 +35,23 @@ class ProductController extends Controller
 
     /**
      * La función de índice devuelve una vista con una lista de productos.
-     * 
+     *
      * @return la vista índice de los productos a la cual se le pasa el array productos para que los muestre.
      */
-    public function index() 
+    public function index()
     {
         return view("product.index")->with("products", $this->products);
     }
 
     /**
      * Recupera un producto específico de una matriz y lo pasa a una vista para su visualización.
-     * 
+     *
      * @param string $id El identificador del producto que se mostrará.
-     * 
+     *
      * @return una vista llamada show que recibe el producto de la petición para que lo serialice.
      */
-    public function show(string $id) 
+    public function show(string $id)
     {
-        $product = null;
-
-        foreach ($this->products as $key => $value) {
-            if ($key == $id) 
-            {
-                $product = $value;
-                break;
-            }
-        }
-    
-        return view("product.show")->with("product", $product);
+        return view("product.show")->with("product", $this->products[$id]);
     }
 }
